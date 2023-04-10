7f941678f83e5e3404dc8260ab11bce3afea5181
Katelyn B <kebreit@gmail.com>
2023-04-10 10:01:15 -0500

    Implemented unit tests for Subtract, Multiply, Divide, Power

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,6 +30,101 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_Valid_Katelyn_Breitbarth()
+        {
+            Assert.AreEqual(-3, Program.Subtract("-1", "2"));
+            Assert.AreEqual(5, Program.Subtract("7", "2"));
+            Assert.AreEqual(9, Program.Subtract("7", "-2"));
+        }
+        
+        [Test]
+        public void Subtract_Invalid_Katelyn_Breitbarth()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null_Katelyn_Breitbarth()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_Valid_Katelyn_Breitbarth()
+        {
+            Assert.AreEqual(10, Program.Multiply("5", "2"));
+            Assert.AreEqual(-10, Program.Multiply("5", "-2"));
+            Assert.AreEqual(0, Program.Multiply("7", "0"));
+        }
+
+        [Test]
+        public void Multiply_Invalid_Katelyn_Breitbarth()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_Katelyn_Breitbarth()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_Valid_Katelyn_Breitbarth()
+        {
+            Assert.AreEqual(10, Program.Divide("100", "10"));
+            Assert.AreEqual(0.5, Program.Divide("2", "4"));
+            Assert.AreEqual(12, Program.Divide("-6", "-2"));
+        }
+
+        [Test]
+        public void Divide_Invalid_Katelyn_Breitbarth()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null_Katelyn_Breitbarth()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        [Test]
+        public void Power_Valid_Katelyn_Breitbarth()
+        {
+            Assert.AreEqual(4, Program.Power("2", "2"));
+            Assert.AreEqual(0.25, Program.Power("2", "-2"));
+            Assert.AreEqual(2, Program.Power("2", "0"));
+        }
+
+        [Test]
+        public void Power_Invalid_Katelyn_Breitbarth()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_Null_Katelyn_Breitbarth()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
+
     }
 }
